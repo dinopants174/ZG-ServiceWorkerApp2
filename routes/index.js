66d6af9c1e31da54e3dfc18ebd60ca41ae25333e
zoherghadyali <zoghadya@microsoft.com>
2019-06-28 00:51:49 -0400

    Hid Facebook login for demo tomorrow

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 var home = function(req, res, next){
-  if (req.user){
-    console.log(req.user);
-    res.render('home', {name: req.user.displayName});
-  } else {
-    res.render('login');
-  }
+  res.render('home', {name: 'Edge DevTools'});
+  // if (req.user){
+  //   console.log(req.user);
+  //   res.render('home', {name: req.user.displayName});
+  // } else {
+  //   res.render('login');
+  // }
 };
 
 module.exports.home = home;
